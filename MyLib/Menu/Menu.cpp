d2ec10952af7ae550e12a736aec877b9376beafc
Borodin <servikb1@gmail.com>
2025-04-23 10:59:37 +0700

    Обновление ANSI

--- MyLib/Menu/Menu.cpp
+++ MyLib/Menu/Menu.cpp
@@ -1,1 +1,26 @@
 #include "Menu.h"
+Menu& Menu::addItem(std::string el, std::function<void()> f){
+	if (!el.empty() && f && !visible) {
+		elMenu.push_back(el);
+		func.push_back(f);
+		count++;
+		calcWidth(el);
+}
+	return*this;
+}
+Menu& Menu::setHead(std::string h) {
+	if (!h.empty() && !visible) {
+		head = h;
+		calcWidth(h);
+	}
+	return*this;
+}
+void Menu::drawBorder() {
+	system("chcp 1251> nul");
+	setCursorPosition(posX, posY);
+	std::cout << Board_LT2;
+	for (int i = 0; i < length; i++)
+	{
+
+	}
+}
--- MyLib/Menu/Menu.h
+++ MyLib/Menu/Menu.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../console/ANSI.h"
 #include <string>
 #include <vector>
 #include <functional>
@@ -11,9 +12,25 @@ private:
 	int cur;
 	int count;
 	int width;
+	bool visible = false;
 	std::string head;
 	std::vector<std::string> elMenu;
-	std::vector<std::function<void()>> functions;
+	std::vector<std::function<void()>> func;
+
+	void strinToVector(std::string s) { if(!s.empty()) elMenu.push_back(s); }
+	void functionToVector(std::function<void()> f) { if (f) func.push_back(f); }
+	void calcWidth(std::string s) { width = std::max( width,(int)s.length() + 2); }
+	void drawBorder();
 public:
-	Menu() : posX(1), posY(1), cur(0), count(0), width(0), head(), elMenu(), functions() {}
+	Menu() : posX(1), posY(1), cur(0), count(0), width(0), head(), elMenu(), func() {}
+	Menu(int x, int y, int cur, int count, int width, std::string h, std::string el, std::function<void()> f)
+		: posX(x), posY(y), cur(0), count(1), width(0)	{
+		setHead(h);
+		strinToVector(el);
+		functionToVector(f);
+	}
+	Menu(int x, int y, std::string h) :Menu(x, y, 0, 0, 0, h, "", nullptr) {}
+	Menu& addItem(std::string el, std::function<void()> f);
+	Menu& setHead(std::string h);
+
 };
